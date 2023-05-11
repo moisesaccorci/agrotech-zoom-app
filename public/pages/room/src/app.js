011103635abbc7365a55a20a8cea3cb500c846b2
Moises <moisesaccorci@gmail.com>
2023-05-11 11:25:18 -0300

    novos testes

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -1,36 +1,36 @@
 const onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const room = urlParams.get('room');
-  console.log('this is the room', room)
+  const room = urlParams.get("room");
+  console.log("this is the room", room);
 
-  const socketUrl = 'http://localhost:3000'
+  const socketUrl = "server-production-5ccc.up.railway.app";
   // const socketUrl = 'https://boiling-eyrie-34493.herokuapp.com'
-  const socketBuilder = new SocketBuilder({ socketUrl })
+  const socketBuilder = new SocketBuilder({ socketUrl });
 
   const peerConfig = Object.values({
     id: undefined,
     config: {
-      // host: 'frozen-river-53219.herokuapp.com',
-      // secure: true,
+      // host: "peer-production.up.railway.app",
+      secure: false,
+      //port: 7376,
       port: 9000,
-      host: 'localhost',
-      path: '/'
-    }
-  })
-  const peerBuilder = new PeerBuilder({ peerConfig })
+      host: "localhost",
+      path: "/",
+    },
+  });
+  const peerBuilder = new PeerBuilder({ peerConfig });
 
-  const view = new View()
-  const media = new Media()
+  const view = new View();
+  const media = new Media();
   const deps = {
     view,
     media,
     room,
     socketBuilder,
-    peerBuilder
-  }
+    peerBuilder,
+  };
 
-  Business.initialize(deps)
+  Business.initialize(deps);
+};
 
-}
-
-window.onload = onload
+window.onload = onload;
